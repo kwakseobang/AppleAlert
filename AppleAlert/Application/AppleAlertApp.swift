e002bce217951c4e288d7f92c3c02303e308f1bf
kwakseobang <iii6602@naver.com>
2024-09-07 17:25:27 +0900

    first

---
+++ AppleAlert/Application/AppleAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleAlertApp.swift
+//  AppleAlert
+//
+//  Created by 곽서방 on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
